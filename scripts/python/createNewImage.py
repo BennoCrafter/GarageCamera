a3a63ae6f34a04fb7de772170f38d35b138009e8
Benno <bennocrafterdev@googlemail.com>
2024-04-18 15:53:29 +0200

    fixed fswebcam

--- scripts/python/createNewImage.py
+++ scripts/python/createNewImage.py
@@ -35,10 +35,14 @@ def capture_image_cv(image_path, image_name, image_type):
 
 
 def capture_image_pi(image_path, image_name, image_type, img_data):
+    # todo --scale causes conflics
+    # command = f"fswebcam {image_path}/{image_name} --scale {str(img_data['scale']['width'])}x{str(img_data['scale']['height'])} --{image_type}"
+
     if image_type in valid_image_formats:
-        command = f"fswebcam {image_path}/{image_name} --scale {str(img_data['scale']['width'])}x{str(img_data['scale']['height'])} --{image_type}"
+        command = f"fswebcam --{image_type} --save {image_path}/{image_name}.{image_type} --{image_type}"
+
     else:
-        command = f"fswebcam {image_path}/{image_name} --scale {str(img_data['scale']['width'])}x{str(img_data['scale']['height'])} --jpeg"
+        command = f"fswebcam --{image_type} --save {image_path}/{image_name}.jpeg"
         print(f"Capture image: Error: Wrong image type. '{image_type}' is not a valid format. Using jpeg instead.")
 
     # Execute the command using os.system()
