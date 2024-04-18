8770e2eeae9d2df91a8d6ef5a7e2a227d35a30fe
Benno <bennocrafterdev@googlemail.com>
2024-04-18 16:08:34 +0200

    fixed image name

--- scripts/python/update.py
+++ scripts/python/update.py
@@ -10,11 +10,12 @@ from scripts.python.runHomeAssistantScript import sendImageToHomeAssistant
 configData = Config("config/config.yaml")
 
 path = configData.get_value("general", "imagePath")
-image_name = datetime.now().strftime("%Y%m%d_%H%M%S")
 image_type = configData.data["general"]["image_data"]["type"]
 image_data = configData.data["general"]["image_data"]
 
 def update():
+    # set image name to time
+    image_name = datetime.now().strftime("%Y%m%d_%H%M%S")
     print(capture_image(path, image_name, image_type, image_data))
     file_names = list_files(path)
     print(manageImageStorage(file_names))
