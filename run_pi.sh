4a85ae720abe5e040ddc9cdab0dd4a6cd1f13de4
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-04-18 10:37:23 +0200

    Update run_pi.sh

--- run_pi.sh
+++ run_pi.sh
@@ -15,11 +15,11 @@ cleanup() {
 trap cleanup SIGINT
 
 echo "Starting Discord Bot"
-python3.11 discord/discordBot.py &
+python3.11 discordBot.py &
 PID1=$!
 
 echo "Starting Server!"
-python3.11 server/server.py &
+python3.11 server.py &
 PID2=$!
 
 
