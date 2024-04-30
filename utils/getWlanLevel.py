b426206f7e0dd5c8289dfe89a27a4ac04c0a1185
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-04-30 18:00:08 +0200

    Update getWlanLevel.py

--- utils/getWlanLevel.py
+++ utils/getWlanLevel.py
@@ -7,7 +7,7 @@ def get_signal_level(wanted_wlan="wlan0"):
         result = subprocess.check_output(["iwconfig", wanted_wlan]).decode("utf-8")
         
         # Use regular expression to find signal level
-        match = re.search(r"Signal level=(-\d+)", result)
+        match = re.search(r"Signal level=(\d+(?:\.\d+)?)(?=\s*\/)", result)
         
         if match:
             signal_level = int(match.group(1))
