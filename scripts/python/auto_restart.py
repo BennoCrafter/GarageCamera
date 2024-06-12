1162c45526f0a68f19c818efaa958f7b3e18ba54
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-06-12 21:44:15 +0200

    Added auto restart script

---
+++ scripts/python/auto_restart.py
@@ -0,0 +1,17 @@
+# TODO: add config for when to reboot and sleep time
+
+import os
+import time
+from datetime import datetime
+
+def reboot():
+    os.system('sudo reboot')
+
+def should_reboot():
+    current_time = datetime.now().time()
+    return current_time.hour == 4 and current_time.minute == 0
+
+while True:
+    if should_reboot():
+        reboot()
+    time.sleep(10)
