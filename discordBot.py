3c4a55f73f0614feb4413ae8fb902187bd10cea9
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-06-15 13:20:23 +0200

    Update discordBot.py
    Missing :

--- discordBot.py
+++ discordBot.py
@@ -61,7 +61,7 @@ async def on_message_create(ctx):
         await ctx.send(embed=await embed_template("info", f"Terminal\n {output}", ctx.user))
     elif channel_id == watcher_channel_id:
         # send response to watcher
-        if ctx.message.author != bot.author
+        if ctx.message.author != bot.author:
             await watcher_channel.send("pong:" + ctx.message.content)
 
 @slash_command(name="ping", description="Ping command.")
