0d6897f7050384e576e5451c292c8876006af737
Benno <bennocrafterdev@googlemail.com>
2024-07-06 17:12:33 +0200

    s

--- discordBot.py
+++ discordBot.py
@@ -31,7 +31,6 @@ if useDiscordBot:
     bot = interactions.Client(token=token, intents=Intents.GUILDS | Intents.ALL)
 else:
     print("Disabled Discord Bot")
-exit(1)
 @listen()
 async def on_ready():
     global status_channel
