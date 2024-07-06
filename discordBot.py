547319a9cd61f08e5ec7ae4a7933e3886951a13d
Benno <bennocrafterdev@googlemail.com>
2024-07-06 17:14:30 +0200

    f

--- discordBot.py
+++ discordBot.py
@@ -57,7 +57,8 @@ async def on_message_create(ctx):
             await ctx.message.delete()
             await refresh_image(ctx)
         if ctx.message.content == "latest":
-            await send_image(sortImages(list_files(configData.get_value("general", "imagePath")))[-1], "AutoDetector")
+            latest_image_name = sortImages(list_files(configData.get_value("general", "imagePath")))[-1],
+            await send_image(f"{configData.get_value("general", "imagePath")}/{latest_image_name}","AutoDetector")
     elif channel_id == terminal_channel_id:
         await ctx.defer(ephemeral=True)
         stream = os.popen(ctx.message.content)
