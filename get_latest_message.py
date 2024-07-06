b387027accee5e99b2ad0ce8fbd4cafbb3093b19
Benno <bennocrafterdev@googlemail.com>
2024-07-06 16:53:59 +0200

    Och komo m

--- get_latest_message.py
+++ get_latest_message.py
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage:  <channelid> <token>")
         exit(0)
-    print(get_latest_discord_message(sys.argv[0], sys.argv[1]))
+    print(get_latest_discord_message(sys.argv[1], sys.argv[2]))
