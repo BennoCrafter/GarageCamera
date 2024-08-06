1706394d490dd7bdffb735259630af1f29565274
Benno <bennocrafterdev@googlemail.com>
2024-08-06 21:34:28 +0200

    smc changeds

--- rotateMotor.py
+++ rotateMotor.py
@@ -17,21 +17,21 @@ GPIO.setup(B, GPIO.OUT)
 GPIO.setup(C, GPIO.OUT)
 GPIO.setup(D, GPIO.OUT)
 
-# Set the GPIO Pin to LOW
+timeout = 0.001
 
 def GPIO_SETUP(a, b, c, d):
     GPIO.output(A, a)
     GPIO.output(B, b)
     GPIO.output(C, c)
     GPIO.output(D, d)
-    time.sleep(0.01)
+    time.sleep(timeout)
 
 def RIGHT_TURN(deg):
     full_circle = 510.0
     degree = full_circle / 360 * deg
     GPIO_SETUP(0, 0, 0, 0)
 
-    while degree > 0:
+    for i in range(int(degree)):
         GPIO_SETUP(1, 0, 0, 0)
         GPIO_SETUP(1, 1, 0, 0)
         GPIO_SETUP(0, 1, 0, 0)
@@ -40,7 +40,6 @@ def RIGHT_TURN(deg):
         GPIO_SETUP(0, 0, 1, 1)
         GPIO_SETUP(0, 0, 0, 1)
         GPIO_SETUP(1, 0, 0, 1)
-        degree -= 1
 
 def LEFT_TURN(deg):
     full_circle = 420.0
